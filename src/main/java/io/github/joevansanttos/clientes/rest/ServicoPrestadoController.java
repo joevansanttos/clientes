f9720ec06087d8a1e99260592fbe0759a431689a
joevansanttos <joevansanttos@gmail.com>
2022-07-23 09:36:03 -0300

    add servico-prestado form

--- src/main/java/io/github/joevansanttos/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/joevansanttos/clientes/rest/ServicoPrestadoController.java
@@ -12,8 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,31 +23,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ServicoPrestadoController {
 
-    private final ClienteRepository clienteRepository;
-    private final ServicoPrestadoRepository repository;
-    private final BigDecimalConverter bigDecimalConverter;
 
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto){
-
-        LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-
-        Integer idCliente = dto.getIdCliente();
-
-        Cliente cliente =  clienteRepository
-                .findById(idCliente)
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.BAD_REQUEST, "Cliente Inexistente!"));
-
-        ServicoPrestado servicoPrestado = new ServicoPrestado();
-        servicoPrestado.setDescricao(dto.getDescricao());
-        servicoPrestado.setData(data);
-        servicoPrestado.setCliente(cliente);
-        servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
-   fv
-        return repository.save(servicoPrestado);
-    }
 
 }
